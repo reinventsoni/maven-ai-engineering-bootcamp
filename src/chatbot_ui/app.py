4088b12b51973a3576e9efa263920560f54e52b8
Sanjay Soni <reinventsoni@gmail.com>
2025-10-03 20:48:22 +1000

    Refactored App with separate Frontend and Backend

---
+++ src/chatbot_ui/app.py
@@ -0,0 +1,76 @@
+from sys import exception
+import streamlit as st
+from openai import OpenAI
+from google import genai
+from groq import Groq
+import requests
+
+from core.config import config
+
+# Let's create a side bar with a dropdown for the model list and providers
+with st.sidebar:
+    st.title("Settings")
+    # Dropdown for model
+    provider = st.selectbox("Provider", ["OpenAI", "Google", "Groq"])
+    if provider == "OpenAI":
+        model_name = st.selectbox("Model", ["gpt-4o-mini", "gpt-4o"])
+    elif provider == "Groq":
+        model_name = st.selectbox("Model", ["llama-3.3-70b-versatile"])
+    else:
+        model_name = st.selectbox("Model", ["gemini-2.0-flash"])
+    # Save Provider and Model name to the session state
+    st.session_state.provider = provider
+    st.session_state.model_name = model_name
+
+def api_call(method, url, **kwargs):
+    def _show_error_popup(message):
+        """Show Error Message as a Popup in the top-right corner"""
+        st.session_state["error_popup"] = {
+            "visible":True,
+            "message":message
+        }
+    try:
+        response = getattr(requests, method)(url, **kwargs)
+        try:
+            response_data = response.json()
+        except requests.exceptions.JSONDecodeError:
+            response_data = {"message": "Invalid JSON response from the API"}
+        
+        if response.ok:
+            return True, response_data
+        return False, response_data
+
+    except requests.exceptions.ConnectionError:
+        _show_error_popup("Connection Error. Please check your network connection")
+        return False, {"message": "Connection Error"}
+    except requests.exceptions.Timeout:
+        _show_error_popup("Request Timeout. Please try again later")
+        return False, {"message": "Request Timeout"}
+    except Exception as e:
+        _show_error_popup(f"An error occurred: {str(e)}")
+        return False, {"message": str(e)}
+  
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+    {
+        "role": "assistant",
+        "content": "Hello! How can I help you today?"
+    }]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+    
+if prompt := st.chat_input("Hello! How can I assist you today?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        output = api_call("post", f"{config.API_URL}/chat", json={"provider": st.session_state.provider, "model_name": st.session_state.model_name, "messages": st.session_state.messages})
+        response_data = output[1]
+        answer = response_data["message"]
+        st.write(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
